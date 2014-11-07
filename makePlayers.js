3aaf1d46187060d6665a185d73381838e2fd63f1
Daniel Bromley <dbromley1@live.co.uk>
2014-11-07 15:52:07 +0000

    Update makePlayers.js

--- makePlayers.js
+++ makePlayers.js
@@ -8,6 +8,12 @@ var player = {
     handLibraryGraveyard = document.createElement('div');
     handLibraryGraveyard.className = 'handLibraryGraveyard';
     this[player_name].appendChild(handLibraryGraveyard);
+    exile = document.createElement('div');
+    graveyard = document.createElement('div');
+    library = document.createElement('div');
+    exile.className = 'pileZone';
+    graveyard.className = 'pileZone';
+    library.className = 'pileZone';
     return this[player_name];
   }
 }
