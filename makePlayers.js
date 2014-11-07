78e87a607c2103e8792120475bc624911e0e0b10
Daniel Bromley <dbromley1@live.co.uk>
2014-11-07 11:17:37 +0000

    Update makePlayers.js

--- makePlayers.js
+++ makePlayers.js
@@ -1,8 +1,9 @@
 var player = {
   addNewPlayer: function(player_name){
-    this[player_name] = {
-      lifeTotal: 20,
-      maximumHandSize: 7
-    }
+    this[player_name] = document.createElement('div');
+    this[player_name].className = 'player';
+    this[player_name].id = player_name
+    this[player_name].lifeTotal = 20;
+    this[player_name].maximumHandSize = 7;
   }
 }
