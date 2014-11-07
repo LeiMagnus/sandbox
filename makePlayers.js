3e90acb8e220aca0fc6d54c79b993be01d20f8fd
Daniel Bromley <dbromley1@live.co.uk>
2014-11-07 13:11:43 +0000

    Update makePlayers.js

--- makePlayers.js
+++ makePlayers.js
@@ -5,6 +5,18 @@ var player = {
     this[player_name].id = player_name
     this[player_name].lifeTotal = 20;
     this[player_name].maximumHandSize = 7;
+    exile = document.createElement('div');
+    exile.className = player_name;
+    exile.id = 'exile';
+    graveyard = document.createElement('div');
+    graveyard.className = player_name;
+    graveyard.id = 'graveyard';
+    library = document.createElement('div');
+    library.className = player_name;
+    library.id = 'library';
+    this[player_name].appendChild(exile);
+    this[player_name].appendChild(graveyard);
+    this[player_name].appendChild(library);
     return this[player_name];
   }
 }
